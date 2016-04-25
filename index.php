bffa1a4bc9f0ae1d22ee8ffcb53d0dbf3dc3ca16
Malonias <semhiles@gmail.com>
2016-04-25 22:38:24 +0200

    updated code
    delete button calling function in directive. Deleting not yet fully functional. Add new person functional

--- index.php
+++ index.php
@@ -10,35 +10,51 @@
 </style>
 
 
-<h1> Angular Js First Assignment </h1>
+<h1> Angular Js Assignment 1.1</h1>
 <div class="row" ng-controller="NamesController">
 	
-		<div class="col-sm-4" >
+		<div class="col-sm-8" >
 
 			<input type="text" ng-model="filter.searchText" />
 
 					<br/><br/>Showing people with the following letters in their names:<br/>
 					{{ filter.searchText }}<br/><br/>
-													
-							<ul>
-	  						  <li ng-repeat="munhu in people | filter:filter.searchText | orderBy:'name'">{{ munhu.name }} - {{ munhu.center }}
-	  						  <button class="btn btn-danger" ng-click="deleteMunhu($index)">Delete</button>
-	  						  </li>	
-							</ul>
+					<!-- <table width="100%"  class="table table-striped">
+					 	<tr class="success">
+					 		<th>NAME</th>
+					 		<th>CENTER</th>
+					 		<th>AGE</th>
+					 		<th>OCCUPATION</th>
+					 		<th>&nbsp</th>
+					 	</tr>
+					 	
+					 	<person>
+					 	</person>
+					
+					 </table>
+
+						-->	
+						<ul>
+							<li ng-repeat="munhu in people | filter:filter.searchText | orderBy:'name'">
+								<ng-person deletep="deleteMunhu(i)" person="munhu" i="$index"></ng-person>
+							</li>
+						</ul>
 														
 		</div>
 		
-		<div class="col-sm-4">
+		<div class="col-sm-2">
 						<h3>
 							<b>Add new entry</b>
 						</h3>
 						<br/><br/>
+						<p class="bg-danger text-primary" style="float: center">{{msbg}}</p>
 						Name  &nbsp:&nbsp
-						<input type="text" ng-model="newMunhu.name" /><br/><br>
+						<input type="text" ng-model="newMunhu.zita" /><br/><br>
 						Center :&nbsp
-						<input type="text" ng-model="newMunhu.centre" /><br/><br/>
+						<input type="text" ng-model="newMunhu.center" /><br/><br/>
 
 						<button class="btn btn-success" ng-click="addMunhu()">Add new entry</button>
+											
 		</div>
 
 </div>

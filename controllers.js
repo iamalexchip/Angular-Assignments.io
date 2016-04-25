b334b598a26478e91336170dd2f62508596c6257
Malonias <semhiles@gmail.com>
2016-04-25 22:41:42 +0200

    calling function in directivetemplate functional

--- controllers.js
+++ controllers.js
@@ -1,32 +1,59 @@
 
 var demoApp = angular.module('demoApp', [])
 
-.controller('NamesController', function ($scope){
+
+demoApp.directive('ngPerson', function() {
+
+	return {
+		restrict: 'E',
+		scope: {
+			person: '=',
+			i: '=',
+			deletep: '&'
+		},
+
+	replace:true,
+
+	template: '<p>{{ person.name }} - {{ person.center }} - <button ng-click="deletep()">Delete - {{i}}</button></p>',
+
+
+	}// RETURNED SON
+
+})//DIRECTIVE NUMBER TWO
+
+demoApp.controller('NamesController', function ($scope){
 
 	$scope.newMunhu = {name: null, center: null};
-
+    $scope.msbg = 'sss';
+	
 	$scope.people = [
-		{name: 'Alice Muchokomori', center:'Highfield'},
-		{name: 'Allen Lindegard', center:'Pretoria'},
-		{name: 'James Makombero', center:'Headquaters'},
-		{name: 'Jimmy Gadzikayi', center:'Gutu'},
-		{name: 'Paul Nzombe', center:'Mutare'},
-		{name: 'Patricia Nzombe', center:'Mutare'},
-		{name: 'Gregory Chirumhanzu', center:'Belverdere'},
-		{name: 'Taura Donje', center:'Msasa'}
+		{name: 'Alice Muchokomori', center:'Highfield', occupation:' Touble maker',age: 10},
+		{name: 'Allen Lindegard', center:'Pretoria', occupation:'Real Estate Agent ',age: 35},
+		{name: 'James Makombero', center:'Headquaters', occupation:'Mechanic',age: 40},
+		{name: 'Jimmy Gadzikayi', center:'Gutu', occupation:'Primary School Teacher',age: 23},
+		{name: 'Paul Nzombe', center:'Mutare', occupation:'Commercial Farmer',age: 52},
+		{name: 'Patricia Nzombe', center:'Mutare', occupation:'EMA Chairman',age: 34},
+		{name: 'Gregory Chirumhanzu', center:'Belverdere', occupation:'Neurologist',age: 37},
+		{name: 'Taura Donje', center:'Msasa', occupation:'Phycologist',age: 30}
 	];
 
 	$scope.addMunhu = function (){
-		
-		$scope.people.push({
-			name: $scope.newMunhu.name, 
-			center: $scope.newMunhu.centre,
-		});
-	};// finit addMunhu
-	
-	 $scope.deleteMunhu = function(index){
-   		$scope.people.splice(index, 1);
-};
+			
+		if ( $scope.newMunhu.zita != null && $scope.newMunhu.center != null ){
 
-// end of controlers
-});
+			$scope.people.push({
+				name: $scope.newMunhu.zita, 
+				center: $scope.newMunhu.center
+			});
+
+		}else{
+			alert('Enter in both fields');
+		}//endif
+	};// finit addMunhu
+
+		$scope.deleteMunhu = function(i){
+   		$scope.people.splice(i, 1);
+    }
+
+})// end of controlers
+
